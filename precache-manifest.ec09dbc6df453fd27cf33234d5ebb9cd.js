41d1496aaa796c2390a1dc45baad418bd1a6866a
Pardeg <pardeg@gmail.com>
2020-09-29 12:29:13 +0300

    Updates

---
+++ precache-manifest.ec09dbc6df453fd27cf33234d5ebb9cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5803553597e2dfb3bb584ca8677f831e",
+    "url": "/exel-table/index.html"
+  },
+  {
+    "revision": "e93c2f10fa93703a4c14",
+    "url": "/exel-table/static/css/main.8c824727.chunk.css"
+  },
+  {
+    "revision": "59ec06ee38b6d676ca33",
+    "url": "/exel-table/static/js/2.d8fbbdc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exel-table/static/js/2.d8fbbdc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93c2f10fa93703a4c14",
+    "url": "/exel-table/static/js/main.b651978d.chunk.js"
+  },
+  {
+    "revision": "875a8bb226fc4ecb10ed",
+    "url": "/exel-table/static/js/runtime-main.00453550.js"
+  }
+]);
